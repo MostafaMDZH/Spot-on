f79e60dd6fe649ab049a7df6bd67d8a74af43584
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-07-10 10:05:35 +0430

    Info: some minor refactoring

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,5 +1,4 @@
-import React from "react";
-import Chart from "../Chart/Chart";
+import Chart from '../Chart/Chart';
 import './Info.css';
 
 export default function Info({
@@ -10,16 +9,26 @@ export default function Info({
 
     //numberWithCommas:
     const numberWithCommas = (number) => {
-        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
+    //getPerDistance:
+    const getPerDistance = () => {
+        return numberWithCommas(Number(data.perDistance < 0 ? -data.perDistance : data.perDistance).toFixed(2))
+    }
+
+    //getPerTime:
+    const getPerTime = (value) => {
+        return numberWithCommas(value < 0 ? -value : value);
     }
 
     //setDefaultData:
     const getDefaultData = () => {
         return {
             perDistance: '0.00',
-            daily:   '00.0',
-            monthly: '00',
-            yearly:  '00',
+            daily:       '00.0',
+            monthly:     '00',
+            yearly:      '00',
             chart: [
                 { name: 'Dep', title: 'Depreciation'           , value: 0 },
                 { name: 'T&I', title: 'Tax & Insurance'        , value: 0 },
@@ -30,39 +39,43 @@ export default function Info({
         }
     }
 
+    //startup:
     if(data === null)
         data = getDefaultData();
     
     //render:
     return (
-        <div className="Info">
-            <div className="costs">
-                <a className="title">Your costs will be</a>
-                <div className="distanceWrapper">
-                    <a className="unit">{currency[1]}</a>
-                    <a className="ammount">{numberWithCommas(data.perDistance)}</a>
-                    <a className="period">per {distance[0].toLowerCase()}</a>
+        <div className='Info'>
+            <div className='costs'>
+                <h5 className='title'>Your <a
+                    className={data.perDistance >= 0 ? '' : 'benefits'}
+                    >{data.perDistance >= 0 ? 'costs' : 'benefits'}</a> will be</h5>
+                <div className='distanceWrapper'>
+                    <a className='unit'>{currency[1]}</a>
+                    <a className='ammount' title={getPerDistance()}>{getPerDistance()}</a>
+                    <a className='period'>per {distance[0].toLowerCase()}</a>
                 </div>
-                <div className="timeWrapper">
-                    <div className="timeCostWrapper">
-                        <a className="period">daily</a>
-                        <div className="ammountWrapper">
-                            <a className="currency">{currency[1]}</a>
-                            <a className="ammount">{numberWithCommas(data.daily)}</a>
+                <div className='timeWrapper'>
+                    <div className='timeCostWrapper'>
+                        <a className='period'>daily</a>
+                        <div className='ammountWrapper'>
+                            <a className='currency'>{currency[1]}</a>
+                            <a className='ammount' title={getPerTime(data.daily)}>{getPerTime(data.daily)}
+                            </a>
                         </div>
                     </div>
-                    <div className="timeCostWrapper">
-                        <a className="period">monthly</a>
-                        <div className="ammountWrapper">
-                            <a className="currency">{currency[1]}</a>
-                            <a className="ammount">{numberWithCommas(data.monthly)}</a>
+                    <div className='timeCostWrapper'>
+                        <a className='period'>monthly</a>
+                        <div className='ammountWrapper'>
+                            <a className='currency'>{currency[1]}</a>
+                            <a className='ammount' title={getPerTime(data.monthly)}>{getPerTime(data.monthly)}</a>
                         </div>
                     </div>
-                    <div className="timeCostWrapper">
-                        <a className="period">yearly</a>
-                        <div className="ammountWrapper">
-                            <a className="currency">{currency[1]}</a>
-                            <a className="ammount">{numberWithCommas(data.yearly)}</a>
+                    <div className='timeCostWrapper'>
+                        <a className='period'>yearly</a>
+                        <div className='ammountWrapper'>
+                            <a className='currency'>{currency[1]}</a>
+                            <a className='ammount' title={getPerTime(data.yearly)}>{getPerTime(data.yearly)}</a>
                         </div>
                     </div>
                 </div>
