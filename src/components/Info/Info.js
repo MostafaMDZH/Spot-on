fa4cea75f92b2f8356228c961c64fc3aac804d2b
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-07-16 18:09:06 +0430

    Info: updated

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -33,7 +33,7 @@ export default function Info({
                 { name: 'Dep', title: 'Depreciation'           , value: 0 },
                 { name: 'T&I', title: 'Tax & Insurance'        , value: 0 },
                 { name: 'W&M', title: 'Warranty & Maintenance' , value: 0 },
-                { name: 'Ser', title: 'Servce'                 , value: 0 },
+                { name: 'Ser', title: 'Service'                , value: 0 },
                 { name: 'Fue', title: 'Fuel'                   , value: 0 },
             ]
         }
@@ -46,41 +46,60 @@ export default function Info({
     //render:
     return (
         <div className='Info'>
-            <div className='costs'>
-                <h5 className='title'>Your <a
-                    className={data.perDistance >= 0 ? '' : 'benefits'}
-                    >{data.perDistance >= 0 ? 'costs' : 'benefits'}</a> will be</h5>
-                <div className='distanceWrapper'>
+            <div className='container'>
+
+                {/* title */}
+                <div className='titleWrapper'>
+                    <div className='smallText'>
+                        <a className='your'>Your</a>
+                        <a className='ownership'>ownership</a>
+                    </div>
+                    <div className='bigText'>
+                        <a className={'costs ' + (data.perDistance >= 0 ? '' : 'benefits')}>
+                            {data.perDistance >= 0 ? 'costs' : 'benefits'}
+                        </a>
+                        <a className='willBe'>will be</a>
+                    </div>
+                </div>
+
+                {/* per distance */}
+                <div className='perDistanceWrapper'>
                     <a className='unit'>{currency[1]}</a>
-                    <a className='ammount' title={getPerDistance()}>{getPerDistance()}</a>
+                    <a className='amount' title={getPerDistance()}>{getPerDistance()}</a>
                     <a className='period'>per {distance[0].toLowerCase()}</a>
                 </div>
-                <div className='timeWrapper'>
+
+                {/* per time */}
+                <div className='perTimeWrapper'>
                     <div className='timeCostWrapper'>
                         <a className='period'>daily</a>
-                        <div className='ammountWrapper'>
+                        <div className='amountWrapper'>
                             <a className='currency'>{currency[1]}</a>
-                            <a className='ammount' title={getPerTime(data.daily)}>{getPerTime(data.daily)}
+                            <a className='amount' title={getPerTime(data.daily)}>{getPerTime(data.daily)}
                             </a>
                         </div>
                     </div>
                     <div className='timeCostWrapper'>
                         <a className='period'>monthly</a>
-                        <div className='ammountWrapper'>
+                        <div className='amountWrapper'>
                             <a className='currency'>{currency[1]}</a>
-                            <a className='ammount' title={getPerTime(data.monthly)}>{getPerTime(data.monthly)}</a>
+                            <a className='amount' title={getPerTime(data.monthly)}>{getPerTime(data.monthly)}</a>
                         </div>
                     </div>
                     <div className='timeCostWrapper'>
                         <a className='period'>yearly</a>
-                        <div className='ammountWrapper'>
+                        <div className='amountWrapper'>
                             <a className='currency'>{currency[1]}</a>
-                            <a className='ammount' title={getPerTime(data.yearly)}>{getPerTime(data.yearly)}</a>
+                            <a className='amount' title={getPerTime(data.yearly)}>{getPerTime(data.yearly)}</a>
                         </div>
                     </div>
                 </div>
+
+                {/* chart */}
+                <div className='chartWrapper'>
+                    <Chart currency = {currency} data = {data.chart}/>
+                </div>
             </div>
-            <Chart currency = {currency} data = {data.chart}/>
         </div>
     )
 
